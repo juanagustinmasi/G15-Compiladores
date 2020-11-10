7dc446be2ca92c50f563a8be359fc91fd9ec745a
Marcelo Gabriel Lorenz <marcelo.lorenz@hotmail.com>
2020-11-10 15:53:49 -0300

    Update pruebaDIM.bat

--- Grupo15/Lotes de Prueba/pruebaDIM.bat
+++ Grupo15/Lotes de Prueba/pruebaDIM.bat
@@ -1,8 +1,8 @@
 flex ../Lexico.l
 bison -dyv ../sintactico.y
-gcc lex.yy.c y.tab.c -o programa.exe
+gcc lex.yy.c y.tab.c -o segunda.exe
 pause
-programa.exe pruebaDIM.txt
+segunda.exe pruebaDIM.txt
 del lex.yy.c
 del y.tab.c
 del y.output
