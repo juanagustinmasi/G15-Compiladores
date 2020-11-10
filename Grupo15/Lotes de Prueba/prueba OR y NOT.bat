c0b5dab4bb8b6153654b1ea407f76228c7a61bfa
Marcelo Gabriel Lorenz <marcelo.lorenz@hotmail.com>
2020-11-10 15:53:09 -0300

    Update prueba OR y NOT.bat

--- Grupo15/Lotes de Prueba/prueba OR y NOT.bat
+++ Grupo15/Lotes de Prueba/prueba OR y NOT.bat
@@ -1,8 +1,8 @@
 flex ../Lexico.l
 bison -dyv ../sintactico.y
-gcc lex.yy.c y.tab.c -o programa.exe
+gcc lex.yy.c y.tab.c -o segunda.exe
 pause
-programa.exe "prueba--OR y NOT.txt"
+segunda.exe "prueba--OR y NOT.txt"
 del lex.yy.c
 del y.tab.c
 del y.output
